8a2ac65aed7add5f46df4a9943568a9d71d747a9
Muhamad Luqman Shamsuddin <luqsam1@gmail.com>
2017-06-05 04:33:20 +0800

    Spawning Zombie, needs to work with number of it and spawning of it and AI of it ( really need a lot of workaround).

---
+++ Assets/Script/SpawningZombies.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningZombies : MonoBehaviour {
+
+	public Transform[] spawnPoints;
+	public GameObject[] whatToSpawnPrefabs;
+	public GameObject[] whatToSpawnClones;
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if(other.tag == "Particle")
+		{
+			Destroy (gameObject, 0.02f);
+		}
+
+		whatToSpawnClones[0] = Instantiate(whatToSpawnPrefabs[0], spawnPoints[0].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+		whatToSpawnClones[1] = Instantiate(whatToSpawnPrefabs[1], spawnPoints[1].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+		whatToSpawnClones[2] = Instantiate(whatToSpawnPrefabs[2], spawnPoints[2].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+		whatToSpawnClones[3] = Instantiate(whatToSpawnPrefabs[3], spawnPoints[3].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+	}
+}

b562935081f95acef1bad76f3e3f6ef51f1d5e84
FattyMieo <fong2009@live.com.my>
2017-06-11 23:54:48 +0800

    Revamped MainMenu

---
+++ Assets/MainMenu/SoundManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public List<AudioSource> BGM;
+    public List<AudioSource> SoundFX;
+
+    void Start()
+    {
+        TrackAudio();
+    }
+
+    void TrackAudio()
+    {
+        foreach(GameObject go in GameObject.FindGameObjectsWithTag("BGM"))
+        {
+            BGM.Add(go.GetComponent<AudioSource>());
+        }
+        foreach(GameObject go in GameObject.FindGameObjectsWithTag("SoundFX"))
+        {
+            SoundFX.Add(go.GetComponent<AudioSource>());
+        }
+    }
+}
